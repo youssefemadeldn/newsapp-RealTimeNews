886437039f12deba4e9a4de1aa4bcbef95f97420
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-18 00:18:49 +0300

    add your request

--- lib/servcies/news_services.dart
+++ lib/servcies/news_services.dart
@@ -7,6 +7,5 @@ class NewsService {
   void getService() async {
     Response response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=77b8f7df8a524fdd95bf90b76b3b98d2');
-    print(response);
   }
 }
