301efcf94dbc67ba56a826cad9d2d678860a2d40
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-18 00:46:39 +0300

    Receive the  target data

--- lib/servcies/news_services.dart
+++ lib/servcies/news_services.dart
@@ -7,7 +7,9 @@ class NewsService {
   void getService() async {
     Response response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=77b8f7df8a524fdd95bf90b76b3b98d2');
+
     Map<String, dynamic> jesonData = response.data;
-    print(jesonData['articles']);
+    List<dynamic> articles = jesonData['articles'];
+    print(articles);
   }
 }
