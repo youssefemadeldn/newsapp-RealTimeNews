f8452a5e592971ee4c9592672dc2859a4d15a408
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-18 00:42:12 +0300

    accsess another part of jesonData

--- lib/servcies/news_services.dart
+++ lib/servcies/news_services.dart
@@ -8,6 +8,6 @@ class NewsService {
     Response response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=77b8f7df8a524fdd95bf90b76b3b98d2');
     Map<String, dynamic> jesonData = response.data;
-    print(jesonData['totalResults']);
+    print(jesonData['articles']);
   }
 }
