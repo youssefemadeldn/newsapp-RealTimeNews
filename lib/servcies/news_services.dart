bc9f53fee962e6109f561b3e16cd21f1f69ee924
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-18 18:14:51 +0300

    add return type to getService() methode, finish

--- lib/servcies/news_services.dart
+++ lib/servcies/news_services.dart
@@ -5,7 +5,7 @@ class NewsService {
   final Dio dio;
   NewsService(this.dio);
 
-  void getService() async {
+  Future<List<NewsModel>> getService() async {
     Response response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=77b8f7df8a524fdd95bf90b76b3b98d2');
     // the general structure to receive data from Api
@@ -21,6 +21,7 @@ class NewsService {
           subTitil: article['description']);
       articlesList.add(newsModel);
     }
-    print(articlesList);
+    // print(articlesList);
+    return articlesList;
   }
 }
