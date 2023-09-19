ed8c4ad065d3050d5d57cdb06a68cfc31ad365c0
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-19 15:31:43 +0300

    create NewsTypeView

---
+++ lib/views/news_type_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsTypeView extends StatelessWidget {
+  const NewsTypeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
