16c78afd465cc345924b691df70fbaea9bf05160
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-19 18:00:54 +0300

    Display data in NewsTypeView

--- lib/views/news_type_view.dart
+++ lib/views/news_type_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/widgets/news_list_view_request.dart';
 
 class NewsTypeView extends StatelessWidget {
   const NewsTypeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          NewsListViewRequest(),
+        ],
+      ),
+    );
   }
 }
